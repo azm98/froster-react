79cee5e20c08e65ef36de05a02d91a7ee2c3b7e2
azm haque <azmhaque.98@gmail.com>
2021-07-01 21:26:10 +0500

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header-top">
+                <div className="container">
+                    <div className="header-top-content">
+                        <div className="col-lg-8 col-md-7 col-sm-12 col-xs-12">
+                            <div className="header-content-info">
+                                <ul>
+                                    <li>
+                                    <i className="flaticon-telephone">
+                                        Customer Support:+923350327579
+                                    </i>
+                                    </li>
+                                    <li>
+                                    <i className="flaticon-interface-1">
+                                        azmhaque.98@gmail.com
+                                    </i>
+                                    </li>
+                                </ul>
+                           
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/indexlayout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class indexlayout extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
